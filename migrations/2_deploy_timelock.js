ee9b75ff5e2b1e8a6ad0b5fce57a76e52997a82e
defifofum <defifofum0@gmail.com>
2021-09-21 19:52:04 -0400

    refactor: Timelock migration

--- migrations/2_deploy_timelock.js
+++ migrations/2_deploy_timelock.js
@@ -1,11 +1,31 @@
-
 const Timelock = artifacts.require("Timelock");
+
+const { getDeployConfig } = require("../deploy-config");
 
 
 module.exports = async function (deployer, network, accounts) {
-  // TODO: Configure 
-  const minDelay = 10;
-  const proposers = [accounts[1]];
-  const executors = [accounts[1]];
-  await deployer.deploy(Timelock, minDelay, proposers, executors);
+  const [ deployerAccount ] = accounts;
+  const { minDelay, admin, additionalProposers, additionalExecutors } = getDeployConfig(network, accounts);
+
+  // Setup roles
+  const proposers = [admin, ...additionalProposers];
+  const executors = [admin, ...additionalExecutors];
+  // Deploy timelock
+  await deployer.deploy(Timelock, minDelay, proposers, executors, {from: deployerAccount});
+  const timelock = await Timelock.at(Timelock.address);
+  // Transfer ownership to general admin
+  const TIMELOCK_ADMIN_ROLE = await timelock.TIMELOCK_ADMIN_ROLE();
+  await timelock.grantRole(TIMELOCK_ADMIN_ROLE, admin, {from: deployerAccount});
+  await timelock.renounceRole(TIMELOCK_ADMIN_ROLE, deployerAccount, {from: deployerAccount});
+  // Verify results
+  const adminHasRole = await timelock.hasRole(TIMELOCK_ADMIN_ROLE, admin, {from: deployerAccount});
+  const deployerHasRole = await timelock.hasRole(TIMELOCK_ADMIN_ROLE, deployerAccount, {from: deployerAccount});
+
+  // Log/verify results
+  console.dir({
+    timelock: timelock.address,
+    admin,
+    adminHasRole,
+    deployerHasRole
+  })
 };
