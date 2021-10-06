0351bfae6af2f582f51d1d27c29ae8a8dde48033
defifofum <defifofum0@gmail.com>
2021-10-06 18:27:18 -0400

    feat: Add MasterApeAdminEncoder - Encode txs with this addtional encoder - Use along with TimelockEncoder to send txs through the timelock - Add an example script

--- lib/constants/addressList.ts
+++ lib/constants/addressList.ts
@@ -1,6 +1,7 @@
 interface AddressList {
     OZ_TIMELOCK_ALPHA: string;
     MASTER_APE_DUMMY: string;
+    MASTER_APE_ADMIN_DUMMY: string;
     MESSAGE_BOARD: string;
 }
 
@@ -8,6 +9,7 @@ export const addressList: Record<number, AddressList> = {
     56: {
         OZ_TIMELOCK_ALPHA: '0xED1EE002970805Cd8b0270BEA58907327e867f29',
         MASTER_APE_DUMMY: '0x7C2d70FC900b289A82428f47da2504428Fc92dcB',
+        MASTER_APE_ADMIN_DUMMY: '0x5458ce70A7ef85f46Fa4b2503DD25b03AA85B60b',
         MESSAGE_BOARD: '0x968697b58a792ac43c43D7F6997F1044De38ddE8',
     }
 }
