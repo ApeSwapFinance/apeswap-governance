a34ef1ee9337c4f60b6a7abb88bbdc0f3d70c455
defifofum <defifofum0@gmail.com>
2022-11-29 16:54:50 -0500

    fix: Script name

--- scripts/encodeTransferMasterApeOwnershipTx.ts
+++ scripts/encodeTransferMasterApeOwnershipTx.ts
@@ -9,7 +9,10 @@ import MasterApeAdminEncoder from '../lib/timelock/MasterApeAdminEncoder';
         const salt = BYTES_32(String(Math.floor(Math.random() * 100000) + 1));
         const masterApeAdminAddress = addressList[56].MASTER_APE_ADMIN_DUMMY;
         // NOTE: Mainnet test OZ timelock owner of MasterApeAdmin Dummy
+        // MasterApeAdmin Owner address (can set pending owner of MasterApe)
         const timelockAddress = addressList[56].OZ_TIMELOCK_BRAVO;
+        // NOTE: Set delay based on timelock settings
+        const TIMELOCK_DELAY = 3600 * 6 // 20 for dummy contracts
         const pendingOwner = addressList[56].GENERAL_ADMIN_EOA;
         const timelockEncoder = new TimelockEncoder(timelockAddress);
         const masterApeAdminEncoder = new MasterApeAdminEncoder(masterApeAdminAddress);
@@ -17,23 +20,25 @@ import MasterApeAdminEncoder from '../lib/timelock/MasterApeAdminEncoder';
         const setPendingOwnerEncoded = await masterApeAdminEncoder.encodeSetPendingMasterApeOwner(pendingOwner);
         const setPendingOwnerTimelockEncoded = await timelockEncoder.encodeTxsForSingleOperation(
             { 
-                target: masterApeAdminAddress, value: '0', 
+                target: masterApeAdminAddress, 
+                value: '0', 
                 data: setPendingOwnerEncoded.data || '0x', 
                 predecessor: BYTES_32_0, 
                 salt: salt  
             }, 
-            20 // NOTE: Set delay based on timelock setting
+            TIMELOCK_DELAY
             )
         // 2. Accept pending owner
         const acceptMasterApeOwnership = await masterApeAdminEncoder.encodeAcceptMasterApeOwnership();
         const acceptMasterApeOwnershipTimelockEncoded = await timelockEncoder.encodeTxsForSingleOperation(
             { 
-                target: masterApeAdminAddress, value: '0', 
+                target: masterApeAdminAddress, 
+                value: '0', 
                 data: acceptMasterApeOwnership.data || '0x', 
                 predecessor: BYTES_32_0, 
                 salt: salt  
             }, 
-            20 // NOTE: Set delay based on timelock setting
+            TIMELOCK_DELAY
         )
 
         console.dir({SetPendingOwnerTimelockEncoded: setPendingOwnerTimelockEncoded, AcceptMasterApeOwnershipTimelockEncoded: acceptMasterApeOwnershipTimelockEncoded}, { depth: 5 });
